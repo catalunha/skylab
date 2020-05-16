c94b74245c2ce050c01def1ac4047548f5551e84
catalunha <catalunha.mj@gmail.com>
2020-05-16 11:17:02 -0300

    api com axios no html

--- main.js
+++ main.js
@@ -8,30 +8,40 @@
 //   }
 // }
 
-var minhaPromise = function () {
-  return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest()
-    xhr.open('GET', 'https://api.github.com/users/catalunha')
-    xhr.send(null)
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText))
-        } else {
-          reject('Erro na requisição')
-        }
-      }
-    }
-  })
-}
+// var minhaPromise = function () {
+//   return new Promise(function (resolve, reject) {
+//     var xhr = new XMLHttpRequest()
+//     xhr.open('GET', 'https://api.github.com/users/catalunha')
+//     xhr.send(null)
+//     xhr.onreadystatechange = function () {
+//       if (xhr.readyState === 4) {
+//         if (xhr.status === 200) {
+//           resolve(JSON.parse(xhr.responseText))
+//         } else {
+//           reject('Erro na requisição')
+//         }
+//       }
+//     }
+//   })
+// }
 
-// var res = minhaPromise()
-// console.log(res)
+// // var res = minhaPromise()
+// // console.log(res)
 
-minhaPromise()
+// minhaPromise()
+//   .then(function (responseResolve) {
+//     console.log(responseResolve)
+//   })
+//   .catch(function (responseReject) { 
+//     console.log(responseReject)
+//   })
+
+axios.get('https://api.github.com/users/catalunha')
   .then(function (responseResolve) {
-    console.log(responseResolve)
+    // console.log(responseResolve)
+    console.log(responseResolve.data.avatar_url)
   })
   .catch(function (responseReject) { 
     console.log(responseReject)
   })
+
