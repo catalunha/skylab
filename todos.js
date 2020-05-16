ea53416e0730c3acc0fb50bfab3084ae58800895
catalunha <catalunha.mj@gmail.com>
2020-05-16 10:43:16 -0300

    salvando todos

--- todos.js
+++ todos.js
@@ -1,1 +1,45 @@
-alert('todos')
+var listElement = document.querySelector('#app ul')
+var inputElement = document.querySelector('#app input')
+var buttonElement = document.querySelector('#app button')
+
+// var todos = ['Comer', 'Programar', 'Brincar com as kids']
+var todos = JSON.parse(localStorage.getItem('listTodos')) || []
+
+function renderTodos() {
+  listElement.innerHTML = ''
+  for (let todo of todos) {
+    var todoElement = document.createElement('li')
+    var todoText = document.createTextNode(todo + '  ')
+    var linkElement = document.createElement('a')
+    linkElement.setAttribute('href', '#')
+    var pos = todos.indexOf(todo)
+    linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')')
+    var linkText = document.createTextNode('Excluir')
+    linkElement.appendChild(linkText)
+    todoElement.appendChild(todoText)
+    todoElement.appendChild(linkElement)
+    listElement.appendChild(todoElement)
+  }
+}
+
+renderTodos()
+
+function addTodo() {
+  var todoText = inputElement.value
+  todos.push(todoText)
+  inputElement.value = ''
+  renderTodos()
+  saveStorage()
+}
+
+function deleteTodo(pos) {
+  console.log(pos)
+  todos.splice(pos, 1)
+  renderTodos()
+  saveStorage()
+}
+
+function saveStorage() {
+  localStorage.setItem('listTodos', JSON.stringify(todos))
+}
+
