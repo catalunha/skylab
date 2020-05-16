9dbcec2a3534e3b9ed74a0e5588e4c2a615a7e87
catalunha <catalunha.mj@gmail.com>
2020-05-16 11:10:27 -0300

    promise

---
+++ main.js
@@ -0,0 +1,37 @@
+// var xhr = new XMLHttpRequest()
+// xhr.open('GET', 'https://api.github.com/users/catalunha')
+// xhr.send(null)
+
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === 4) {
+//     console.log(JSON.parse(xhr.responseText))
+//   }
+// }
+
+var minhaPromise = function () {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', 'https://api.github.com/users/catalunha')
+    xhr.send(null)
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText))
+        } else {
+          reject('Erro na requisição')
+        }
+      }
+    }
+  })
+}
+
+// var res = minhaPromise()
+// console.log(res)
+
+minhaPromise()
+  .then(function (responseResolve) {
+    console.log(responseResolve)
+  })
+  .catch(function (responseReject) { 
+    console.log(responseReject)
+  })
