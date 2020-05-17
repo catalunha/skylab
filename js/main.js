f3d405f7963aeab1cdcd11826697e4343a08ed8d
catalunha <catalunha.mj@gmail.com>
2020-05-17 16:20:37 -0300

    desestruturacao

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
-function soma(a=1,b=1){
-  return a+b
+const usuario = {
+  nome: 'Diego',
+  idade: 23,
+  endereco: {
+    cidade: 'Rio do sul',
+    estado: "SC"
+  }
 }
 
-console.log(soma(1,2))
-console.log(soma())
+// const nome = usuario.nome
+// const idade = usuario.idade
+// const cidade = usuario.endereco.cidade
+
+const { nome, idade, endereco: { cidade } } = usuario
+
+console.log(nome)
+console.log(idade)
+console.log(cidade)
+
+
+function mostraNome(usuario) {
+  console.log(usuario.nome)
+}
+mostraNome(usuario)
+
+function mostraNome2({nome}) {
+  console.log(nome)
+}
+mostraNome2(usuario)
