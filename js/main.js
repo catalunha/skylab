e97e219836190e3071e1b93f30578b4024a63146
catalunha <catalunha.mj@gmail.com>
2020-05-17 16:08:36 -0300

    arrow function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,15 @@
 const arr = [1, 3, 4, 5, 8, 9]
-const newArr = arr.map((item) => {
+const newArr = arr.map(function (item) {
   return item * 2
 })
 console.log(newArr)
-
-const sumArr = arr.reduce((total, next) => {
-  return total + next
+const newArr2 = arr.map((item) => {
+  return item * 2
 })
-
-console.log(sumArr)
-
-const filterArr = arr.filter((item) => {
-  return item % 2 === 0
+console.log(newArr2)
+const newArr3 = arr.map(item => {
+  return item * 2
 })
-
-console.log(filterArr)
-
-const findArr = arr.find((item) => {
-  return item === 4
-})
-console.log(findArr)
+console.log(newArr3)
+const newArr4 = arr.map(item => item * 2)
+console.log(newArr4)
