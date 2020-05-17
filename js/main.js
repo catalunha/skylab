faac509fdedcc3b18bd062de12fb65ad88fa336b
catalunha <catalunha.mj@gmail.com>
2020-05-17 16:11:01 -0300

    valores padroes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
-const arr = [1, 3, 4, 5, 8, 9]
-const newArr = arr.map(function (item) {
-  return item * 2
-})
-console.log(newArr)
-const newArr2 = arr.map((item) => {
-  return item * 2
-})
-console.log(newArr2)
-const newArr3 = arr.map(item => {
-  return item * 2
-})
-console.log(newArr3)
-const newArr4 = arr.map(item => item * 2)
-console.log(newArr4)
+function soma(a=1,b=1){
+  return a+b
+}
+
+console.log(soma(1,2))
+console.log(soma())
