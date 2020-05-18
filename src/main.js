ea9b884ff33272e445888859c87f895352c50ade
catalunha <catalunha.mj@gmail.com>
2020-05-18 15:01:58 -0300

    exercicio

--- src/main.js
+++ src/main.js
@@ -1,14 +1,59 @@
-import axios from 'axios'
+// // Execicio 1
+// const delay = () => new Promise(resolve => setTimeout(resolve, 1000))
 
-class Api {
-  static async getUserInfo(username) {
-    try {
-      const response = await axios.get(`https://api.github.com/users/${username}`)
-      console.log(response)
-    } catch (error) {
-      console.log('Erro na api do github')
-    }
+// // function umPorSegundo(){
+// //   delay().then(()=>{
+// //     console.log('1s')
+// //     delay().then(()=>{
+// //       console.log('2s')
+// //       delay().then(()=>{
+// //         console.log('3s')
+// //       })
+// //     })
+// //   })
+// // }
+// // umPorSegundo()
+
+
+// async function umPorSegundo2(){
+//   await delay()
+//   console.log('1s')
+//   await delay()
+//   console.log('2s')
+//   await delay()
+//   console.log('3s')
+// }
+// umPorSegundo2()
+
+// Exercicio 2
+
+// import axios from 'axios'
+// function getUserFromGithub(user){
+//   axios.get(`https://api.github.com/users/${user}`)
+//   .then(response=>{
+//     console.log(response.data)
+//   })
+//   .catch(error=>{
+//     console.log('Usuario nao existe')
+//   })
+// }
+
+// getUserFromGithub('catalunha')
+// getUserFromGithub('catalunha2')
+
+
+
+import axios from 'axios'
+async function getUserFromGithub(user) {
+  try {
+    const data = await axios.get(`https://api.github.com/users/${user}`)
+    console.log(data)
+  } catch (error) {
+    console.log('Usuario nao existe')
+
   }
+
 }
 
-const user = Api.getUserInfo('catalunha')
+getUserFromGithub('catalunha')
+getUserFromGithub('catalunha2')
